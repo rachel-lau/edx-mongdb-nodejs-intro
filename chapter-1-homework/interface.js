3d8704f478bb22a0adc3e73ca262a971dd4c333e
Rachel Lau <rachel.yw.lau@gmail.com>
2015-11-26 18:18:52 -0800

    First commit

---
+++ chapter-1-homework/interface.js
@@ -0,0 +1,20 @@
+/*
+ *  Inserts "doc" into the collection "movies".
+ */
+exports.insert = function(db, doc, callback) {
+  // TODO: implement
+  db.collection('movies').insert(doc)
+  callback()
+};
+
+/*
+ *  Finds all documents in the "movies" collection
+ *  whose "director" field equals the given director,
+ *  ordered by the movie's "title" field. See
+ *  http://mongodb.github.io/node-mongodb-native/2.0/api/Cursor.html#sort
+ */
+exports.byDirector = function(db, director, callback) {
+  // TODO: implement
+  var query = { 'director': director }
+  db.collection('movies').find(query).sort({"title":1}).toArray(callback)
+};
